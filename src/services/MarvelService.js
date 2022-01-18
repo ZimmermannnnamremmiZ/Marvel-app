cefea4058685dfc19e7d8cd6458667b12ce5291d
ZimmermannnnamremmiZ <Maximvo92@gmail.com>
2022-01-19 02:20:45 +0300

    comics output limit,add error msg if comics.length === 0

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,7 +13,7 @@ class MarvelService {
     }
 
     getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?offset=222&limit=9&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
